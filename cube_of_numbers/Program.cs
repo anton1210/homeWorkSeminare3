faa12766afd02fde78c787b58a26e1b204ff0f9e
Anton Maslov <anton-maslov3@mail.ru>
2023-02-22 09:50:29 +0300

    Кубы чисел от 1 до N

--- cube_of_numbers/Program.cs
+++ cube_of_numbers/Program.cs
@@ -3,4 +3,11 @@
 и выдаёт таблицу кубов чисел от 1 до N. (3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125) */
 
-
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+for (int i = 1; i <= num; i++)
+{
+    int a = i*i*i;
+    Console.WriteLine(a);
+}
